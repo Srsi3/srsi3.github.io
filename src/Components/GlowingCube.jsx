45e526b4de1c1178896cfdda814aa7ce5f82e34b
srsi3 <ssaldanha03@gmail.com>
2024-10-17 15:52:58 -0400

    minor cube component edits

--- src/Components/GlowingCube.jsx
+++ src/Components/GlowingCube.jsx
@@ -1,8 +1,8 @@
-// src/GlowingCube.js
+
 import React, { useRef, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
+
 import { useFrame } from '@react-three/fiber';
-import { MeshDistortMaterial, Html } from '@react-three/drei';
+import { MeshDistortMaterial} from '@react-three/drei';
 
 const GlowingCube = () => {
   const meshRef = useRef();
@@ -27,21 +27,10 @@ const GlowingCube = () => {
     <mesh ref={meshRef} onClick={handleClick} scale={isUnwrapped ? [1, 0.1, 1] : [1, 1, 1]}>
       <boxGeometry args={[1, 1, 1]} />
       <MeshDistortMaterial color="cyan" attach="material" distort={0.5} speed={2} />
-      <Html center>
-        <div style={{ color: 'white', fontSize: '2em' }}>Click Me!</div>
-      </Html>
     </mesh>
   );
 };
 
-const App = () => {
-  return (
-    <Canvas style={{ height: '100vh' }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <GlowingCube />
-    </Canvas>
-  );
-};
 
-export default App;
+
+export default GlowingCube;
